d878a1a8a3a7d9398791a3b3a39b9639e750dca2
kaiuri <uriel.acioli@gmail.com>
2022-07-12 11:40:50 -0400

    Initial commit

---
+++ src/kaiuri/yahoo_finance.clj
@@ -0,0 +1,72 @@
+(ns kaiuri.yahoo-finance
+  (:require [clojure.string :as string])
+  (:import [java.net URLEncoder]
+           [java.time LocalDate ZoneId]))
+
+(set! *warn-on-reflection* true)
+
+(defn- str->milli
+  [s]
+  (let [date (LocalDate/parse s)
+        zone (ZoneId/systemDefault)
+        milli (-> date
+                  (.atStartOfDay zone)
+                  .toInstant
+                  .toEpochMilli)]
+    (quot milli 1000)))
+
+(defn- query1-v7
+  "PRIVATE: Uses the current (12-07-22) finance.yahoo.com routing to get historical data."
+  [{:keys [stock
+           ^clojure.lang.PersistentVector period
+           frequency]}]
+  (let [stock-chunk (URLEncoder/encode stock)
+        period-chunk (->> period
+                          (map (fn [p]
+                                 (let [idx (+ 1 (.indexOf period p))]
+                                   (str "period" idx \= (str->milli p)))))
+                          (string/join \&))
+        frequency-chunk (str \1 frequency)]
+    (str "https://query1.finance.yahoo.com/v7/finance/download/" stock-chunk
+         \? period-chunk
+         "&interval=" frequency-chunk
+         "&events=history"
+         "&includeAdjustedClose=true")))
+
+(defn- csv->clj
+  "PRIVATE: Subject to changes.
+   Parses CSV string into vector of dicts.
+    - <s>: CSV string.
+    - [del]: Delimiter, defaults to `,`
+
+  "
+  [s & {:keys [del] :or {del #","}}]
+  (let [csv (string/split-lines s)
+        head (map keyword (string/split (first csv) del))
+        body (map #(string/split % del) (rest csv))]
+    (mapv #(zipmap head %) body)))
+
+(defn get
+  "Takes a dict with the following keys:
+    - <stock>: Stock's name (string) as displayed in `finance.yahoo.com`.
+    - <period>: PersistentVector tuple of date strings, each in the format \"YYYY-MM-dd\".
+    - [frequency]: \"w\"(week), \"d\"(day) or \"m\"(month).
+    - [extension]: One of `:clojure`, `:raw`, defaults to `:clojure`.
+
+   Returns CSV string of historical data.
+   "
+
+  [{:keys [stock
+           ^clojure.lang.PersistentVector period
+           frequency
+           extension]
+    :or {frequency "d" extension :clojure}}]
+  (let [raw-data (->> {:stock stock
+                       :period period
+                       :frequency frequency}
+                      (query1-v7)
+                      (slurp))]
+    (case extension
+      :raw raw-data
+      :clojure (csv->clj raw-data)
+      :else (csv->clj raw-data))))
---
+++ test/kaiuri/yahoo_finance_test.clj
@@ -0,0 +1,8 @@
+(ns kaiuri.yahoo-finance-test
+  (:require [clojure.test :refer :all]
+            [kaiuri.yahoo-finance :refer :all]))
+
+;; TODO: write tests
+(deftest a-test
+  (testing "WRITE SOME TESTS, DUDE"
+    (is (= 0 0))))
